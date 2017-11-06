f01cae154043226bb1ae50951b01a32056142b74
Chris Paslawski <chris@paslawski.me>
2017-11-05 23:03:08 -0500

    Random OpenMP trial

--- src/Player/AIPlayer.cpp
+++ src/Player/AIPlayer.cpp
@@ -3,7 +3,7 @@
 
 #include <limits>
 #include <algorithm>
-
+#include <vector>
 #include <iostream>
 #include <fstream>
 #include <list>
@@ -45,51 +45,71 @@ Move AIPlayer::Minimax(const Board& board, /*out*/ int& boardValue, const int& c
     // possible moves lead to death
     bool first = true;
 
+    std::vector<Move> possibleMoves(BOARD_SIZE / 2);
+
+    //#pragma omp parallel for
     for (idx_t k = 0; k < BOARD_SIZE; ++k) {
         // for all current player's tokens...
         if (board.GetCell(k) == 2 - isPlayerOne) {
             // for every possible move...
             for (int8_t i : moves) {
-                Move move = Move(k, k + i);
-
-                // we'll check if this move is legit
-                if (gameManager->IsValidMove(board, move, isPlayerOne)) {
-                    // if it is, simulate it with a copy of the board
-                    Board tmp = board;
-                    gameManager->PlayMove(tmp, move, 1 + isPlayerOne, true);
-
-                    // TODO: store outcome from PlayMove and don't recurse if not necessary
-
-                    // perform minimax on this new result board
-                    // we get the bubbled-up "best" value possible from this configuration
-                    // and keep the move if's better for this level (depending on min/max)
-                    int bv = !isMaxLevel ? std::numeric_limits<int>::min() : std::numeric_limits<int>::max();
-                    Minimax(tmp, bv, currentDepth + 1, maxDepth, !isPlayerOne, !isMaxLevel, alpha, beta);
-
-                    if ((isMaxLevel && bv > boardValue)
-                        || (!isMaxLevel && bv < boardValue)
-                        || first) {
-                        // this move is better than any we've seen so far from the lower levels, keep it
-                        bestMove = move;
-                        boardValue = bv;
-                        first = false;
-
-                        // update alpha and beta values depending on which level we are (min/max)
-                        if (isMaxLevel && boardValue > alpha) {
-                            alpha = boardValue;
-                        } else if (!isMaxLevel && boardValue < beta) {
-                            beta = boardValue;
-                        }
-
-                        // prune the rest of this subtree if alpha and beta conflict
-                        if (beta <= alpha) {
-                            return bestMove;
-                        }
-                    }
-                }
+                possibleMoves.emplace_back(k, k + i);
             }
         }
     }
 
+    bool pruned = false;
+    #pragma omp parallel for
+    for (int i = 0; i < possibleMoves.size(); ++i) {
+        const Move& move = possibleMoves[i];
+
+        // we'll check if this move is legit
+        if (gameManager->IsValidMove(board, move, isPlayerOne)) {
+            // if it is, simulate it with a copy of the board
+            Board tmp = board;
+            gameManager->PlayMove(tmp, move, 1 + isPlayerOne, true);
+
+            // TODO: store outcome from PlayMove and don't recurse if not necessary
+
+            // perform minimax on this new result board
+            // we get the bubbled-up "best" value possible from this configuration
+            // and keep the move if's better for this level (depending on min/max)
+            int bv = !isMaxLevel ? std::numeric_limits<int>::min() : std::numeric_limits<int>::max();
+            Minimax(tmp, bv, currentDepth + 1, maxDepth, !isPlayerOne, !isMaxLevel, alpha, beta);
+
+            if ((isMaxLevel && bv > boardValue)
+                || (!isMaxLevel && bv < boardValue)
+                || first) {
+                omp_set_lock(&lock);
+
+                // this move is better than any we've seen so far from the lower levels, keep it
+                bestMove = move;
+                boardValue = bv;
+                first = false;
+
+                // update alpha and beta values depending on which level we are (min/max)
+                if (isMaxLevel && boardValue > alpha) {
+                    alpha = boardValue;
+                } else if (!isMaxLevel && boardValue < beta) {
+                    beta = boardValue;
+                }
+
+                omp_unset_lock(&lock);
+
+                // prune the rest of this subtree if alpha and beta conflict
+                if (beta <= alpha) {
+                    #pragma omp cancel for
+                }
+            }
+        }
+
+        #pragma omp cancellation point for
+    }
+
     return bestMove;
+}
+
+AIPlayer::~AIPlayer()
+{
+    omp_destroy_lock(&lock);
 }
--- src/Player/AIPlayer.h
+++ src/Player/AIPlayer.h
@@ -2,12 +2,19 @@
 
 #include "Player.h"
 
+#include <omp.h>
+
 #define DEPTH 3
 
 class AIPlayer : public Player
 {
+private:
+    omp_lock_t lock;
 public:
-    AIPlayer() : Player() {};
+    AIPlayer() : Player() {
+        omp_init_lock(&lock);
+    };
+    virtual ~AIPlayer();
     explicit AIPlayer(const bool& playerOne) : Player(playerOne) {};
     Move GetMove(Board board, /*out*/ int* value) override;
     virtual int EvaluateHeuristic(const Board& board) = 0;
