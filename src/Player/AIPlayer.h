8d2bcb8001cdbaf3c4d2df1f77f8f049538ca62d
Christopher Paslawski <chris@paslawski.me>
2017-11-06 15:27:04 -0500

    Add CLI args
    Team 11 - Bonzee
    Usage:
      Armadillo [OPTION...]
    
      -h, --help            Help
      -w, --wait            Wait for debugger to attach
      -g, --green arg       Green player type
      -r, --red arg         Red player type
      -d, --depth arg       Tree search depth for both players (shortcut)
      -G, --greendepth arg  Tree search depth for Red player
      -R, --reddepth arg    Tree search depth for Green player

--- src/Player/AIPlayer.h
+++ src/Player/AIPlayer.h
@@ -3,15 +3,13 @@
 #include "Player.h"
 #include "Node.h"
 
-#define DEPTH 3
-
 class AIPlayer : public Player
 {
 protected:
+    int mDepth;
     int MiniMax(Node* node, int depth, bool max);
 public:
-    AIPlayer() : Player() {};
-    explicit AIPlayer(const bool& playerOne) : Player(playerOne) {};
+    AIPlayer(const bool& playerOne, const int& depth) : Player(playerOne), mDepth(depth) {};
     Move GetMove(Board board, /*out*/ int* value) override;
     virtual int EvaluateHeuristic(const Board& board) = 0;
 };
--- src/Player/HumanPlayer.h
+++ src/Player/HumanPlayer.h
@@ -5,7 +5,6 @@
 class HumanPlayer : public Player
 {
 public:
-    HumanPlayer() : Player() {};
     explicit HumanPlayer(const bool& playerOne) : Player(playerOne) {};
     Move GetMove(Board board, /*out*/ int* value) override;
 };
