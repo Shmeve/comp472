1f5f3ed7c352dd169226fb9b3c5ed74d0070bc09
t-vaudry <tvaudryread@gmail.com>
2017-11-06 17:22:53 -0500

    Cell Moves Player +6 -> Green on black +4 -> Green on white -6 -> Red on black -4 -> Red on white

--- src/PlayerFactory.cpp
+++ src/PlayerFactory.cpp
@@ -6,16 +6,19 @@
 #include "Player/HumanPlayer.h"
 #include "Player/NaivePlayer.h"
 #include "Player/RemainingTilesPlayer.h"
+#include "Player/CellMovesPlayer.h"
 
 const char* typeHumanPlayer = "Human";
 const char* typeNaivePlayer = "Naive Heuristic (D2)";
 const char* typeRemainingTilesPlayer = "Remaining Tiles Player";
+const char* typeCellMovesPlayer = "Cell Moves Player";
 
 const char** PlayerFactory::Options(/*out*/ unsigned int& len)
 {
     static const char* opts[] = {typeHumanPlayer,
                                  typeNaivePlayer,
-                                 typeRemainingTilesPlayer};
+                                 typeRemainingTilesPlayer,
+                                 typeCellMovesPlayer};
 
     len = sizeof(opts) / sizeof(char*);
 
@@ -30,6 +33,8 @@ Player* PlayerFactory::Create(const char* type, const bool& playerOne, const int
         return new NaivePlayer(playerOne, depth);
     } else if (strcmp(type, typeRemainingTilesPlayer) == 0) {
         return new RemainingTilesPlayer(playerOne, depth);
+    } else if (strcmp(type, typeCellMovesPlayer) == 0) {
+        return new CellMovesPlayer(playerOne, depth);
     }
 
     throw std::runtime_error("Invalid player type in PlayerFactory::Create");
